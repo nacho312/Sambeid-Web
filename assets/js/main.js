3a06163ab4aac295e5797363de4ae29141f590d2
Evening2k <iarteaga@utem.cl>
2022-07-13 01:33:37 -0400

    CAMBIOS FINALES
    Co-Authored-By: D0U5M4N <66190758+D0U5M4N@users.noreply.github.com>
    Co-Authored-By: Ignacio Arias <29764372+nacho312@users.noreply.github.com>
    Co-Authored-By: ElTeclas <72234253+ElTeclas@users.noreply.github.com>
    Co-Authored-By: PlopHat <41564958+PlopHat@users.noreply.github.com>
    Co-Authored-By: Nachuwu <87551931+Nachuwu@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,191 @@ function showCheckboxes() {
       expanded = false;
     }
 }
+(function ($) {
+  $(document).ready(function () {
+    $(document).on("change", ".ingredientes input", function () {
+      var numberOfChecked = $(".ingredientes input:checkbox:checked").length;
+      if (numberOfChecked >= 3) {
+        $(".ingredientes input:not(:checked)").prop("disabled", true);
+      } else {
+        $(".ingredientes input:not(:checked)").removeAttr("disabled", true);
+
+      }
+    });
+  });
+})(jQuery);
+
+function getValue() {
+
+  var checkboxes = 
+      document.getElementsByName('Columna1');// ingredientes
+      var checkboxess = 
+      document.getElementsByName('Columna2');// extras
+    
+      
+
+  var ingredientes = "";
+  var extras = "";
+var canti=0;
+var cante=0;
+var x=1;
+var y=1;
+  for (var i = 0; i < checkboxes.length; i++) { // for para cantidad de ingredientes
+      if (checkboxes[i].checked) {
+      canti++;
+      }
+
+    }
+
+//console.log(checkboxes.length);
+for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+      //console.log(i);
+      if(x==canti)
+      {
+        ingredientes += checkboxes[i].value;
+      }else
+      {
+        ingredientes += checkboxes[i].value + " " + ", ";
+      }
+      x++;
+      }
+
+    } 
+  
+  
+  
+    for (var j = 0; j < checkboxess.length; j++) {
+      if (checkboxess[j].checked == true){
+    //extras += checkboxess[j].value + " " +  " , ";
+    cante++;
+    }
+  }
+for (var j = 0; j < checkboxess.length; j++) {
+      if (checkboxess[j].checked == true){
+    
+    if(y==cante)
+      {
+        extras += checkboxess[j].value;
+      }else
+      {
+        extras += checkboxess[j].value + " " +  " , ";
+      }
+      y++;
+    }
+  }
+
+  let nombreyapellido = document.getElementById('nombreyapellido').value;
+  let cantidadp = document.getElementById('cantidadp').value;
+  let toppingid = document.getElementById('toppingid').value;
+  let consulta = document.getElementById('mensaje').value;
+  let whatsapp= 56965371259;
+  var response = grecaptcha.getResponse();
+
+nombreyapellido=nombreyapellido.trim(); // elimina espacio en blanco
+//console.log(nombreyapellido);
+if(nombreyapellido == "" || nombreyapellido.length < 1 || nombreyapellido == null)
+  {
+    swal("¡Error!", "Rellene el campo de Nombre y Apellido", "warning");
+    return false; 
+
+  }else if(cantidadp == "Seleccione la cantidad de personas")
+  {
+    swal("¡Error!", "Rellene el campo Cantidad de personas", "warning");
+    return false;   
+  }
+  else if(canti==0)
+  {
+    swal("¡Error!", "Rellene los campo de ingredientes", "warning");
+    return false;
+  }
+  else if(response.length == 0)
+  {
+    swal("¡Error!", "Válidar el CAPTCHA. Por favor, chequea antes de continuar", "warning");
+    return false;
+  }
+else{
+if(toppingid=="" && consulta == "" && cante==0) // topping == consulta == cantidad extra == 0
+{
+  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
+,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*.`,'_blank');
+}else if(toppingid=="" && cante==0) //topping == cantidad extra == 0
+{
+  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
+,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes},%20
+Mi consulta es:%20${consulta}.`,'_blank');
+}else if(consulta == "" && cante==0)//consulta == cantidad extra == 0
+{
+  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
+,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
+Topping:%20*${toppingid}*.`,'_blank');
+}
+else if(toppingid=="" && consulta == "") // topping vacio
+{
+  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
+,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
+los ingredientes extras son:%20*${extras}*.`,'_blank');
+  
+}else if(consulta == "") // consulta vacia
+{
+  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
+,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
+los ingredientes extras son:%20*${extras}*%20Topping:%20*${toppingid}*.`,'_blank');
+}else if(toppingid=="") // topping vacio y consulta vacia
+{
+  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
+,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
+los ingredientes extras son:%20*${extras}*, Mi consulta es:%20*${consulta}*.`,'_blank');
+}
+else if(cante==0)
+{
+  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
+,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
+Topping:%20*${toppingid}*, Mi consulta es:%20*${consulta}*`,'_blank');
+}
+else
+var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
+,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*
+,%20los ingredientes extras son:%20*${extras}*%20Topping:%20*${toppingid}*, Mi consulta es:%20*${consulta}*`,'_blank');
+}
+
+  }
+function soloLetras(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    especiales = [8, 37, 39, 46];
+
+    tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if(letras.indexOf(tecla) == -1 && !tecla_especial)
+        return false;
+}
+
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+function Ver()
+ {
+   swal("¿Qué son los Topping?","Son extras que funcionan como acompañamientos para la torta con un precio adicional.");
+ }
