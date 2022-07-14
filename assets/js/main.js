57743906860f23e6eedaff241a114f6ee563e879
Evening2k <iarteaga@utem.cl>
2022-07-14 09:19:10 -0400

    Final
    Co-Authored-By: D0U5M4N <66190758+D0U5M4N@users.noreply.github.com>
    Co-Authored-By: ElTeclas <72234253+ElTeclas@users.noreply.github.com>
    Co-Authored-By: Ignacio Arias <29764372+nacho312@users.noreply.github.com>
    Co-Authored-By: Nachuwu <87551931+Nachuwu@users.noreply.github.com>
    Co-Authored-By: PlopHat <41564958+PlopHat@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,14 @@ const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const contacto = document.querySelector("#pies");
+
+contacto.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+
 menuBtn.onclick = ()=>{
   navbar.classList.add("show");
   menuBtn.classList.add("hide");
@@ -106,7 +114,7 @@ for (var j = 0; j < checkboxess.length; j++) {
   let cantidadp = document.getElementById('cantidadp').value;
   let toppingid = document.getElementById('toppingid').value;
   let consulta = document.getElementById('mensaje').value;
-  let whatsapp= 56965371259;
+  let whatsapp= 56998315624;
   var response = grecaptcha.getResponse();
 
 nombreyapellido=nombreyapellido.trim(); // elimina espacio en blanco
@@ -126,74 +134,155 @@ if(nombreyapellido == "" || nombreyapellido.length < 1 || nombreyapellido == nul
     swal("¡Error!", "Rellene los campo de ingredientes", "warning");
     return false;
   }
-  else if(response.length == 0)
+  /*else if(response.length == 0)
   {
     swal("¡Error!", "Válidar el CAPTCHA. Por favor, chequea antes de continuar", "warning");
     return false;
-  }
+  }*/
 else{
-if(toppingid=="" && consulta == "" && cante==0) // topping == consulta == cantidad extra == 0
-{
-  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
-,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*.`,'_blank');
-}else if(toppingid=="" && cante==0) //topping == cantidad extra == 0
-{
-  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
-,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes},%20
-Mi consulta es:%20${consulta}.`,'_blank');
-}else if(consulta == "" && cante==0)//consulta == cantidad extra == 0
-{
-  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
-,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
-Topping:%20*${toppingid}*.`,'_blank');
-}
-else if(toppingid=="" && consulta == "") // topping vacio
-{
-  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
-,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
-los ingredientes extras son:%20*${extras}*.`,'_blank');
+  if(toppingid=="" && consulta == "" && cante==0) // topping == consulta == cantidad extra == 0
+  {
+    if(canti ==1)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, El ingrediente a eleccion es:%20${ingredientes}.`,'_blank');
+    }else if(canti ==2)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los dos ingredientes a eleccion son:%20${ingredientes}.`,'_blank');
+    }else
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes}.`,'_blank');
+    }
+    
+    
+  }else if(toppingid=="" && cante==0) //topping == cantidad extra == 0
+  {
+    if(canti ==1)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, El ingrediente a eleccion es:%20${ingredientes}, 
+  Mi consulta es:%20${consulta}.`,'_blank');  
+    }else if(canti ==2)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los dos ingredientes a eleccion son:%20${ingredientes}, 
+  Mi consulta es:%20${consulta}.`,'_blank');
+    }else
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes}, 
+  Mi consulta es:%20${consulta}.`,'_blank');
+    }
+    
+  }else if(consulta == "" && cante==0)//consulta == cantidad extra == 0
+  {
+    if(canti ==1)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, El ingrediente a eleccion es:%20${ingredientes}
+  Topping:%20${toppingid}.`,'_blank');  
+    }else if(canti ==2)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los dos ingredientes a eleccion son:%20${ingredientes}
+  Topping:%20${toppingid}.`,'_blank');
+    }else
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes}
+  Topping:%20${toppingid}.`,'_blank');  
+    }
+    
+  }
+  else if(toppingid=="" && consulta == "") // topping vacio
+  {
+    if(canti ==1)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, El ingrediente a eleccion es:%20${ingredientes}
+  los ingredientes extras son:%20${extras}.`,'_blank'); 
+    }else if(canti ==2)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los dos ingredientes a eleccion son:%20${ingredientes}
+  los ingredientes extras son:%20${extras}.`,'_blank');
+    }else
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes}
+  los ingredientes extras son:%20${extras}.`,'_blank');
+    }
+    
+    
+  }else if(consulta == "") // consulta vacia
+  {
+    if(canti ==1)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, El ingrediente a eleccion es:%20${ingredientes}
+  los ingredientes extras son:%20${extras}%20Topping:%20${toppingid}.`,'_blank'); 
+    }else if(canti ==2)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los dos ingredientes a eleccion son:%20${ingredientes}
+  los ingredientes extras son:%20${extras}%20Topping:%20${toppingid}.`,'_blank');
+    }else
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes}
+  los ingredientes extras son:%20${extras}%20Topping:%20${toppingid}.`,'_blank');
+    }
+    
+  }else if(toppingid=="") // topping vacio y consulta vacia
+  {
+    if(canti ==1)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, El ingrediente a eleccion es:%20${ingredientes}
+  los ingredientes extras son:%20${extras}, Mi consulta es:%20${consulta}.`,'_blank');  
+    }else if(canti ==2)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los dos ingredientes a eleccion son:%20${ingredientes}
+  los ingredientes extras son:%20${extras}, Mi consulta es:%20${consulta}.`,'_blank');
+    }else
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes}
+  los ingredientes extras son:%20${extras}, Mi consulta es:%20${consulta}.`,'_blank');
+    }
+    
+  }
+  else if(cante==0)
+  {
+    if(canti ==1)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, El ingrediente a eleccion es:%20${ingredientes}
+  Topping:%20${toppingid}, Mi consulta es:%20${consulta}`,'_blank');  
+    }else if(canti ==2)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los dos ingredientes a eleccion son:%20${ingredientes}
+  Topping:%20${toppingid}, Mi consulta es:%20${consulta}`,'_blank');
+    }else
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes}
+  Topping:%20${toppingid}, Mi consulta es:%20${consulta}`,'_blank');
+    }
+    
+  }
+  else
+  {
+  if(canti ==1)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, El ingrediente a eleccion es:%20${ingredientes}
+  los ingredientes extras son:%20${extras}%20Topping:%20${toppingid}, Mi consulta es:%20${consulta}`,'_blank');
+  }else if(canti ==2)
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los dos ingredientes a eleccion son:%20${ingredientes}
+  los ingredientes extras son:%20${extras}%20Topping:%20${toppingid}, Mi consulta es:%20${consulta}`,'_blank');
+  }
+    else
+    {
+      var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*,%20quisiera%20una%20torta%20para%20${cantidadp} personas, Los tres ingredientes a eleccion son:%20${ingredientes}
+  los ingredientes extras son:%20${extras}%20Topping:%20${toppingid}, Mi consulta es:%20${consulta}`,'_blank');
+  }
+    }
   
-}else if(consulta == "") // consulta vacia
-{
-  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
-,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
-los ingredientes extras son:%20*${extras}*%20Topping:%20*${toppingid}*.`,'_blank');
-}else if(toppingid=="") // topping vacio y consulta vacia
-{
-  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
-,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
-los ingredientes extras son:%20*${extras}*, Mi consulta es:%20*${consulta}*.`,'_blank');
-}
-else if(cante==0)
-{
-  var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
-,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*,%20
-Topping:%20*${toppingid}*, Mi consulta es:%20*${consulta}*`,'_blank');
-}
-else
-var win= window.open(`https://wa.me/${whatsapp}?text=Hola%20mi%20nombre%20es%20*${nombreyapellido}*
-,%20quisiera%20una%20torta%20para%20*${cantidadp}* personas, Los tres ingredientes a eleccion son:%20*${ingredientes}*
-,%20los ingredientes extras son:%20*${extras}*%20Topping:%20*${toppingid}*, Mi consulta es:%20*${consulta}*`,'_blank');
-}
+
+    }
 
   }
 function soloLetras(e) {
-    key = e.keyCode || e.which;
-    tecla = String.fromCharCode(key).toLowerCase();
-    letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-    especiales = [8, 37, 39, 46];
-
-    tecla_especial = false
-    for(var i in especiales) {
-        if(key == especiales[i]) {
-            tecla_especial = true;
-            break;
-        }
-    }
-
-    if(letras.indexOf(tecla) == -1 && !tecla_especial)
-        return false;
-}
+      key = e.keyCode || e.which;
+      tecla = String.fromCharCode(key).toLowerCase();
+      letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";  
+      if(letras.indexOf(tecla) == -1 )
+          return false;
+  }
 
 var animateButton = function(e) {
 
@@ -214,5 +303,6 @@ for (var i = 0; i < bubblyButtons.length; i++) {
 }
 function Ver()
  {
-   swal("¿Qué son los Topping?","Son extras que funcionan como acompañamientos para la torta con un precio adicional.");
+   swal("¿Qué son los Toppers?","Es una figura creada en base a papelería para perzonalizar la torta, los cuales se pueden adquirir por un precio adicional. Cake toppers hay de muchos tipos, materiales y texturas.");
  }
+
